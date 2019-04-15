ef5036c9cb7ad530d4027e9b3f2fae128bd795c2
Cecilia Pasquale <cecilia.pasquale16@gmail.com>
2019-04-15 12:28:05 -0400

    final

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('BacoNator', false);
+INSERT INTO burgers (name, devoured) VALUES ('BigMac', false);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', false);
+INSERT INTO burgers (name, devoured) VALUES ('4x4', false);
